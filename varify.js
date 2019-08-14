5e5a4fa68cba075dd596b19a8353d5c782e4efad
angus <lang@zjxu.org>
2019-08-14 14:23:31 +0800

    update algorithm

--- varify.js
+++ varify.js
@@ -22,7 +22,7 @@ varying float vQuality;
 
 void main()
 {
-  gl_FragColor = vec4(texture2D(colorMap, vec2(vQuality, 0.5)).rgb, 1.0);
+  gl_FragColor = vec4(texture2D(colorMap, vec2(1.0 - vQuality, 0.5)).rgb, 1.0);
 }
 `;
 
@@ -40,15 +40,18 @@ const material = new THREE.ShaderMaterial({
 
 var v0 = new THREE.Vector3(), v1 = new THREE.Vector3(), v2 = new THREE.Vector3();
 var u0 = new THREE.Vector2(), u1 = new THREE.Vector2(), u2 = new THREE.Vector2();
-function _compute_density(vx0, vx1, vx2, uv0, uv1, uv2) {
+var area_sum = 0;
+function _compute_area(vx0, vx1, vx2, uv0, uv1, uv2, i, texel_area, geometry_area) {
   vx1.sub(vx0);
   vx2.sub(vx0);
-  var areas = vx1.cross(vx2).length();
+  var area = vx1.cross(vx2).length();
+  geometry_area[i] = geometry_area[i + 1] = geometry_area[i + 2] = area;
+  area_sum += area;
+
   uv2.sub(uv0);
   uv1.sub(uv0);
-  // for more percision, times 1000
-  var texels = Math.abs(uv2.cross(uv1)) * 100;
-  return texels / areas;
+  // for more percision, times 100
+  texel_area[i] = texel_area[i + 1] = texel_area[i + 2] =  Math.abs(uv2.cross(uv1)) * 100 / 1.2;
 }
 
 function varify(object) {
@@ -68,59 +71,48 @@ function varify(object) {
   if (!uv) {
     throw new Error('geometry must have uv attribute');
   }
-  
-  var quality;
-  var min_density = Infinity;
-  var max_density = -Infinity;
 
   var positionData = position.array;
   var uvData = uv.array;
-
-  if (!geometry.index) {
-    var vertexCount = position.count;
-    quality = new Float32Array(vertexCount);
-    for (var i = 0; i < vertexCount; i += 3) {
-      v0.fromArray(positionData, i * 3);
-      v1.fromArray(positionData, (i + 1) * 3);
-      v2.fromArray(positionData, (i + 2)  * 3);
-
-      u0.fromArray(uvData, i * 2);
-      u1.fromArray(uvData, (i + 1) * 2);
-      u2.fromArray(uvData, (i + 2) * 2);
-
-      var density = _compute_density(v0, v1, v2, u0, u1, u2);
-      min_density = Math.min(density, min_density);
-      max_density = Math.max(density, max_density);
-      quality[i] = density;
-      quality[i + 1] = density;
-      quality[i + 2] = density;
-    }
-  } else {
-    var indexCount = geometry.index.count;
+  
+  if (geometry.index) {
+    // expand index
     var indexData = geometry.index.array;
-    quality = new Float32Array(indexCount);
-    for (let i = 0; i < indexCount; i += 3) {
-      var f0 = indexData[i], f1 = indexData[i + 1], f2 = indexData[i + 2];
-      v0.fromArray(positionData, f0 * 3);
-      v1.fromArray(positionData, f1 * 3);
-      v2.fromArray(positionData, f2 * 3);
-
-      u0.fromArray(uvData, f0 * 2);
-      u1.fromArray(uvData, f1 * 2);
-      u2.fromArray(uvData, f2 * 2);
-
-      var density = _compute_density(v0, v1, v2, u0, u1, u2);
-      min_density = Math.min(density, min_density);
-      max_density = Math.max(density, max_density);
-      quality[i] = density;
-      quality[i + 1] = density;
-      quality[i + 2] = density;
+    var indexCount = geometry.index.count;
+    var newPosition = new Float32Array(indexCount * 3);
+    var newUv = new Float32Array(indexCount * 2);
+    for (let i = 0; i < indexCount; ++i) {
+      var index_ptr = indexData[i];
+      v0.fromArray(positionData, index_ptr * 3).toArray(newPosition, i * 3);
+      u0.fromArray(uvData, index_ptr * 2).toArray(newUv, i * 2);
     }
+    position.setArray(newPosition);
+    uv.setArray(newUv);
+  
+    positionData = newPosition;
+    uvData = newUv;
   }
 
-  var range = max_density - min_density;
-  for (var i = 0; i < quality.length; ++i) {
-    quality[i] = (quality[i] - min_density) / range;
+  area_sum = 0;
+  var vertexCount = position.count;
+  var geometry_area = new Float32Array(vertexCount);
+  var texel_area = new Float32Array(vertexCount);
+  for (var i = 0; i < vertexCount; i += 3) {
+    v0.fromArray(positionData, i * 3);
+    v1.fromArray(positionData, (i + 1) * 3);
+    v2.fromArray(positionData, (i + 2)  * 3);
+
+    u0.fromArray(uvData, i * 2);
+    u1.fromArray(uvData, (i + 1) * 2);
+    u2.fromArray(uvData, (i + 2) * 2);
+
+    _compute_area(v0, v1, v2, u0, u1, u2, i, texel_area, geometry_area);
+  }
+
+  var quality = new Float32Array(vertexCount);
+  for (let i = 0; i < quality.length; i += 3) {
+    var density = texel_area[i] / (geometry_area[i] / area_sum * 100);
+    quality[i] = quality[i + 1] = quality[i + 2] = density;
   }
 
   geometry.computeBoundingBox();
